801876ef86edd83a05c2ad176072af678e15353d
Lars Grefer <github@larsgrefer.de>
2015-12-01 12:26:30 +0100

    okhttp with configurator

--- modules/okhttp/src/main/java/io/freefair/android/injection/modules/OkHttpModule.java
+++ modules/okhttp/src/main/java/io/freefair/android/injection/modules/OkHttpModule.java
@@ -10,48 +10,49 @@ import java.io.File;
 
 import io.freefair.android.injection.InjectionContainer;
 import io.freefair.android.injection.InjectionModule;
-import io.freefair.android.util.function.CachingSupplier;
+import io.freefair.android.util.function.Consumer;
 import io.freefair.android.util.function.Supplier;
 import io.freefair.android.util.function.Suppliers;
 
 @SuppressWarnings("unused")
 public class OkHttpModule implements InjectionModule {
 
-    private Supplier<Cache> cacheSupplier;
-    private Supplier<OkHttpClient> clientSupplier;
+    private Consumer<OkHttpClient> configurator;
 
-    private Context context;
-
-    public OkHttpModule(Context context){
-        this.context = context;
+    public OkHttpModule(Consumer<OkHttpClient> configurator){
+        this.configurator = configurator;
     }
 
     @Override
     public void configure(InjectionContainer injectionContainer) {
-        ensureValues();
-
-        injectionContainer.registerSupplier(Cache.class, Suppliers.cache(cacheSupplier));
+        injectionContainer.registerSupplier(OkHttpClient.class, Suppliers.cache(new Supplier<OkHttpClient>() {
+            @Nullable
+            @Override
+            public OkHttpClient get() {
+                OkHttpClient client = new OkHttpClient();
+                configurator.accept(client);
+                return client;
+            }
+        }));
     }
 
-    private void ensureValues() {
-        if(cacheSupplier == null)
-            cacheSupplier = new Supplier<Cache>() {
-                @Nullable
-                @Override
-                public Cache get() {
-                    return new Cache(new File(context.getCacheDir(), "okhttp" ), 20 * 1024 * 1024);
-                }
-            };
+    public static OkHttpModule withEmptyConfig(){
+        return new OkHttpModule(new Consumer<OkHttpClient>() {
+            @Override
+            public void accept(OkHttpClient value) {
 
-        if(clientSupplier == null) {
-            clientSupplier = new Supplier<OkHttpClient>() {
-                @Nullable
-                @Override
-                public OkHttpClient get() {
-                    OkHttpClient okHttpClient = new OkHttpClient();
-                    return okHttpClient;
-                }
-            };
-        }
+            }
+        });
+    }
+
+    public static OkHttpModule withCache(final Context context){
+        return new OkHttpModule(new Consumer<OkHttpClient>() {
+            @Override
+            public void accept(OkHttpClient value) {
+                File cacheDir = new File(context.getCacheDir(), "okhttp");
+                long size = 1024 * 1024 * 10;
+                value.setCache(new Cache(cacheDir, size));
+            }
+        });
     }
 }
